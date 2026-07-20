347ecbeb165793050abbabac03a75c6bba477bd8
saladin76 <salahelnabtity@gmail.com>
2026-07-20 09:02:47 +0300

    Activate projects listing route

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import { SiteFooter } from "@/components/layout/site-footer";
+import { SiteHeader } from "@/components/layout/site-header";
+import { StickyDonateBar } from "@/components/layout/sticky-donate-bar";
+import { TopUtilityBar } from "@/components/layout/top-utility-bar";
+import { ProjectsExplorer } from "@/components/projects/projects-explorer";
+import { ProjectsHero } from "@/components/projects/projects-hero";
+import { projectMetrics } from "@/data/project-metrics";
+import { projects } from "@/data/projects";
+
+export default function ProjectsPage(){return <><TopUtilityBar/><SiteHeader/><main id="main-content"><ProjectsHero projects={projects}/><ProjectsExplorer projects={projects} metrics={projectMetrics}/></main><SiteFooter/><StickyDonateBar/></>}
