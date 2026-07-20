a58455552363314676dcb5b43adf2e148f5f585e
saladin76 <salahelnabtity@gmail.com>
2026-07-20 09:48:59 +0300

    Activate project listing detail navigation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,9 +2,10 @@ import { SiteFooter } from "@/components/layout/site-footer";
 import { SiteHeader } from "@/components/layout/site-header";
 import { StickyDonateBar } from "@/components/layout/sticky-donate-bar";
 import { TopUtilityBar } from "@/components/layout/top-utility-bar";
+import { ProjectsDetailLinkEnhancer } from "@/components/projects/projects-detail-link-enhancer";
 import { ProjectsExplorer } from "@/components/projects/projects-explorer";
 import { ProjectsHero } from "@/components/projects/projects-hero";
 import { projectMetrics } from "@/data/project-metrics";
 import { projects } from "@/data/projects";
 
-export default function ProjectsPage(){return <><TopUtilityBar/><SiteHeader/><main id="main-content"><ProjectsHero projects={projects}/><ProjectsExplorer projects={projects} metrics={projectMetrics}/></main><SiteFooter/><StickyDonateBar/></>}
+export default function ProjectsPage(){return <><TopUtilityBar/><SiteHeader/><main id="main-content"><ProjectsHero projects={projects}/><ProjectsExplorer projects={projects} metrics={projectMetrics}/><ProjectsDetailLinkEnhancer projects={projects}/></main><SiteFooter/><StickyDonateBar/></>}
